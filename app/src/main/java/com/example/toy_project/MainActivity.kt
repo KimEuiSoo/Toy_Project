941d6b6a023afc0483909f8a2f2e836edcb69e60
BOOK-1V2KOUUPE2\dmltn <dmltn3426@naver.com>
2023-07-24 19:58:50 +0900

    #project

--- app/src/main/java/com/example/toy_project/MainActivity.kt
+++ app/src/main/java/com/example/toy_project/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.toy_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val name_text = findViewById<EditText>(R.id.buttontext1);
+        val phone_text = findViewById<EditText>(R.id.buttontext2);
+
+        name_text.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(v: View?){
+                name_text.setText("");
+            }
+        })
     }
 }
