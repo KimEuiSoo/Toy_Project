3cafd20d7f14f510b617906f8354687d44e34016
BOOK-1V2KOUUPE2\dmltn <dmltn3426@naver.com>
2023-07-28 00:57:43 +0900

    #내부 저장소 완료

--- app/src/main/java/com/example/toy_project/MainActivity.kt
+++ app/src/main/java/com/example/toy_project/MainActivity.kt
@@ -8,73 +8,74 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import com.example.toy_project.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val btn = findViewById<Button>(R.id.Btn1)
-        val btn2 = findViewById<Button>(R.id.Btn2)
-        val name = findViewById<EditText>(R.id.name)
-        val phone = findViewById<EditText>(R.id.phone)
-
-        btn.setOnClickListener(object: View.OnClickListener{
-            override fun onClick(v: View?){
-                if(Global().Test==1)
-                    Test_Register_Click(name.text.toString(), phone.text.toString())
-                else
-                    Register_Click(name.text.toString(), phone.text.toString())
-            }
-        })
-
-        btn2.setOnClickListener(object: View.OnClickListener{
-            override fun onClick(v: View?){
-                if(Global().Test==1)
-                    Test_getAllDb()
-                else
-                    getAllDb()
-            }
-        })
+    private val binding: ActivityMainBinding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
     }
 
     private val personHelper: PersonDB_Test by lazy{
         PersonDB_Test.getInstance(applicationContext)
     }
 
-    fun Test_Register_Click(name: String, phone: String){
-        val tv = findViewById<TextView>(R.id.text_view)
-        try{
-            personHelper.insertData(
-                name.toString().trim(),
-                phone.toString().trim()
-            )
-            val selectResult = "이름 ${name.toString()}, 전화번호 ${phone.toString()}"
-            tv.append(selectResult + "\n\n")
-            Toast.makeText(this@MainActivity, "등록완료 \n$selectResult", Toast.LENGTH_SHORT).show()
-        }catch (e: Exception){
-            Toast.makeText(this@MainActivity, "등록실패", Toast.LENGTH_LONG).show()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        Register_Click()
+        getAllDb()
+    }
+
+    override fun onDestroy() {
+        personHelper.close()
+        super.onDestroy()
+    }
+
+    private fun showText(text: String){
+        binding.tvResult.append(text+"\n\n")
+    }
+
+
+    fun Register_Click(){
+        binding.Btn1.setOnClickListener{
+            try{
+                if(Global().Test==1){
+                    personHelper.insertData(
+                        binding.name.text.toString().trim(),
+                        binding.phone.text.toString().trim()
+                    )
+                }
+                else{
+
+                }
+                reset()
+                Toast.makeText(this@MainActivity, "등록완료", Toast.LENGTH_SHORT).show()
+            }catch (e: Exception){
+                Toast.makeText(this@MainActivity, "등록실패", Toast.LENGTH_LONG).show()
+            }
         }
+
+
     }
 
-    fun Test_getAllDb() {
-        val tv = findViewById<TextView>(R.id.text_view)
-        try {
-            val selectResult = personHelper.getAllData()
-            tv.append(selectResult + "\n\n")
-        } catch (e: Exception) {
-            e.printStackTrace()
+    fun getAllDb() {
+        binding.Btn2.setOnClickListener {
+            try {
+                val selectResult = personHelper.getAllData()
+                showText(selectResult)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
         }
-    }
-
-    fun getAllDb(){
-
-    }
-
-    fun Register_Click(name: String, phone: String){
-
     }
 
     fun reset(){
+        with(binding){
+            name.setText("")
+            phone.setText("")
+        }
     }
 }
--- app/src/main/java/com/example/toy_project/PersonDB_Test.kt
+++ app/src/main/java/com/example/toy_project/PersonDB_Test.kt
@@ -5,7 +5,92 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class PersonDB_Test private  constructor(context: Context) :
+class PersonDB_Test private constructor(context: Context) :
+    SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    companion object {
+        const val DATABASE_NAME = "Phone.db"
+        const val DATABASE_VERSION = 1
+        const val TABLE_NAME = "phone_test"
+        const val COL1_ID = "_id"
+        const val COL2_NAME = "name"
+        const val COL3_PHONE = "phone"
+
+        //SingleTon Pattern(싱글톤 패턴)
+        @Volatile
+        private var instance: PersonDB_Test? = null
+
+        fun getInstance(context: Context) =
+            instance ?: synchronized(PersonDB_Test::class.java) {
+                instance ?: PersonDB_Test(context).also {
+                    instance = it
+                }
+            }
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        val createQuery = "CREATE TABLE $TABLE_NAME (" +
+                "$COL1_ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "$COL2_NAME TEXT, " +
+                "$COL3_PHONE TEXT )"
+
+        db?.execSQL(createQuery)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        if (oldVersion != newVersion) {
+            db?.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
+            onCreate(db)
+        }
+    }
+
+    fun insertData(name: String, phone: String) {
+        val db = this.writableDatabase
+        val contentValues = ContentValues().apply {
+            put(COL2_NAME, name)
+            put(COL3_PHONE, phone)
+        }
+        db.insert(TABLE_NAME, null, contentValues) // 값이 없으면 행을 삽입하지않음
+    }
+
+    fun updateData(id: String, name: String, phone: String) {
+        val db = this.writableDatabase
+        val contentValues = ContentValues().apply {
+            put(COL1_ID, id)
+            put(COL2_NAME, name)
+            put(COL3_PHONE, phone)
+        }
+        db.update(TABLE_NAME, contentValues, "$COL1_ID = ?", arrayOf(id))
+    }
+
+    fun getAllData(): String {
+        var result = "No data in DB"
+
+        val db = this.readableDatabase
+        val cursor = db.rawQuery("SELECT * FROM $TABLE_NAME", null)
+
+        try {
+            if (cursor.count != 0) {
+                val stringBuffer = StringBuffer()
+                while (cursor.moveToNext()) {
+                    stringBuffer.append("ID :" + cursor.getInt(0).toString() + "\n")
+                    stringBuffer.append("NAME :" + cursor.getString(1) + "\n")
+                    stringBuffer.append("PHONE :" + cursor.getString(2) + "\n")
+                }
+                result = stringBuffer.toString()
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            if (cursor != null && !cursor.isClosed) {
+                cursor.close()
+            }
+        }
+        return result
+    }
+}
+
+/*class PersonDB_Test private  constructor(context: Context) :
     SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSON){
     private var db: SQLiteDatabase? = null
 
@@ -86,4 +171,4 @@ class PersonDB_Test private  constructor(context: Context) :
         }
         return result
     }
-}
+}*/
